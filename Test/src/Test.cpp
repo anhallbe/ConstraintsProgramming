ed63291962122e114a055a6a03c346a85b9cdefb
anhallbe <anhallbe@gmail.com>
2014-03-25 11:27:54 +0100

    Another eclipse commit

--- Test/src/Test.cpp
+++ Test/src/Test.cpp
@@ -11,6 +11,6 @@
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "!!!Hello World!!!" << endl << endl; // prints !!!Hello World!!!
 	return 0;
 }
