e8252883e71ab39aeef1c76217972550cd9b218b
anhallbe <anhallbe@gmail.com>
2014-03-25 10:45:00 +0100

    Another test commit!

---
+++ TestProject/TestProject/TestProject.cpp
@@ -0,0 +1,16 @@
+// TestProject.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include<iostream>
+#include<cstdlib>
+
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout << "Hello world!" << endl;
+	system("PAUSE");
+	return 0;
+}
+
