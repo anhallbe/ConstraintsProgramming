47760913866ff5c03cd4097cbcde1f1f7700cfce
anhallbe <anhallbe@gmail.com>
2014-03-25 11:25:35 +0100

    Eclipse test commit
    asd

---
+++ Test/src/Test.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : Test.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
